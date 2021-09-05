a2328c0da333333e2baa9872f22b71941a4de306
Daniel Alves Mertins <dalvesmertins@gmail.com>
2021-09-05 20:05:25 -0300

    Add method for returning the top n greatest debtors

--- src/contracts.py
+++ src/contracts.py
@@ -12,3 +12,36 @@ class Contract:
 
     def __str__(self) -> str:
         return 'id={}, debt={}'.format(self.id, self.debt)
+
+
+class Contracts:
+    @staticmethod
+    def get_top_n_open_contracts(open_contracts: list[Contract],
+                                 renegotiated_contracts: list[int],
+                                 top_n: int) -> list[int]:
+        """Returns the ids of the top_n greatest debtor contracts which are not
+        renegotiated.
+
+        Args:
+            open_contracts: a list of Contract objects.
+            renegotiated_contracts: a list with the ids of Contracts which are
+             renegotiated.
+            top_n: the number of Contract ids to be returned.
+
+        Returns:
+            A list with Contract ids of the top_n greatest debtors.
+
+        Raises:
+            NegativeTopNError: If top_n is negative.
+        """
+
+        if top_n < 0:
+            raise NegativeTopNError('top_n must be greater than 0.')
+
+        open_contracts.sort(key=lambda contract: contract.debt, reverse=True)
+        return [contract.id for contract in open_contracts if
+                contract.id not in renegotiated_contracts][:top_n]
+
+
+class NegativeTopNError(Exception):
+    pass
